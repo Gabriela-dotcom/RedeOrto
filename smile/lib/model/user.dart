3bf15239ec8aca637f91b63a582f2d6006bed2d6
Tecnico Desenvolvimento de Sistemas <devmat@sistemafieto.rede>
2024-09-11 10:58:46 -0300

    banco de dados

---
+++ smile/lib/model/user.dart
@@ -0,0 +1,11 @@
+class User {
+//criando classe user 
+int? idade; // variavel que aceitar valor null
+String? nome;
+double? peso;
+double? altura;
+bool? acimadopeso;
+int idade1; = 20; //variavel que nao acietar valor null
+Datelime? hora;
+// toda variavel varchar vira string}
+}
